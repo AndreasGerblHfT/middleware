4d988e019cb12582ba1af68fbc037bcbcb3138a4
Andreas Gerbl <41gean1dpt@hft-stuttgart.de>
2025-01-02 17:48:49 +0100

    backend changes for frontend env and cors

---
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Erlaube alle Ursprünge
+        config.addAllowedHeader("*"); // Erlaube alle Header
+        config.addAllowedMethod("*"); // Erlaube alle HTTP-Methoden
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
